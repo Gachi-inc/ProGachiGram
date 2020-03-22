9288940d6e8cefae558da57bab969bf07da4bf78
M1M0G <mixail233442@gmail.com>
2020-03-22 23:20:00 +0500

    Base html

--- src/App.js
+++ src/App.js
@@ -8,15 +8,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Team-2 PROGACHI.
+          <a href = 'https://github.com/frontend-rtf-2020/team-2'> GitHub </a>
+          <ul>
+            <li>Гаев Михаил Алексеевич</li>
+            <li>Овчинников Владимир Олегович</li>
+            <li>Савин Григорий Олегович</li>
+            <li>Паренюк Никита Николаевич</li>
+          </ul>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
