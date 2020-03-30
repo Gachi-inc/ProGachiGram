0aaa0910a68c1b41134d09659ce5de29c9d8f880
M1M0G <mixail233442@gmail.com>
2020-03-30 22:33:58 +0500

    REBUILD 3
    3

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,14 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
-  res.json([{
-    id: 1,
-    username: "first"
-  }, {
-    id: 2,
-    username:"second"
-  }]);
+  res.send('respond with a resource');
 });
 
 module.exports = router;
