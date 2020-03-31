8f2a6c3ebde7931d3493cbede497a72dea2ae884
M1M0G <mixail233442@gmail.com>
2020-03-31 19:38:04 +0500

    add package.json
    1

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+//   });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
