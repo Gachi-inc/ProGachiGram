d1b4a6fc3910f33e8819d1e6b572099e6483ec17
OvchinnikovJMA <DegraDanko@gmail.com>
2020-04-23 03:44:33 +0500

    Продолжается работа над бургером
    На экранах меньше 1000 пока что лучше не включать (работает, но меню хреного отображается)

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
-//import MediaQuery from 'react-responsive'
+import MediaQuery from 'react-responsive'
 import { 
     ActiveNavLink,
     Container,
     Logo,
     NavLink,
     StyledHeader,
+    SpanStyle,
+    In,
+    MenuForPhones,
+    MenuInsides,
 } from './Header.styles';
 
 
 export class Header extends Component{
+    goSlow(){
+        if(this.props.In.checked)
+            {
+                this.props.MenuForPhones.style = "left: 1.9%";
+                this.props.MenuInsides.style = "left: 0";
+            }               
+    }
     render(){
         return(
             <StyledHeader>
                 <Container>
-                    <Logo>ProGachiGram</Logo>
-                    {/*<MediaQuery maxDeviceWidth={1000}>
-                        <input type="checkbox" id="hmt" class="hidden-menu-ticker"/>
-                        <MenuForPhones for="hmt">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </MenuForPhones>
-                        <ul>
+                    {<MediaQuery maxDeviceWidth={1000}>
+                    <In type="checkbox"/>
+                    <MenuForPhones for="hmt">
+                            <SpanStyle></SpanStyle>
+                            <SpanStyle></SpanStyle>
+                            <SpanStyle></SpanStyle>
+                    </MenuForPhones>
+                    <MenuInsides>
                             <NavLink exact to="/" activeStyle={ActiveNavLink}>{'Start'}</NavLink>
                             <NavLink exact to="/app" activeStyle={ActiveNavLink}>{'Messenger'}</NavLink>
                             <NavLink exact to="/registrate" activeStyle={ActiveNavLink}>{'SingIn'}</NavLink>
                             <NavLink exact to="/login" activeStyle={ActiveNavLink}>{'LogIn'}</NavLink>
-                        </ul>
-        </MediaQuery>*/}
+                    </MenuInsides>
+                    <Logo>ProGachiGram</Logo>
+                    </MediaQuery>}
+                    {<MediaQuery minDeviceWidth={1000}>
+                    <Logo>ProGachiGram</Logo>
                     <nav>
                         <NavLink exact to="/" activeStyle={ActiveNavLink}>
                             {'Start'}
@@ -45,8 +58,8 @@ export class Header extends Component{
                         <NavLink exact to="/login" activeStyle={ActiveNavLink}>
                             {'LogIn'}
                         </NavLink>
-                        
-                    </nav>    
+                    </nav> 
+                     </MediaQuery>}      
                 </Container>
             </StyledHeader>
         )
--- client/src/Components/Header/Header.styles.js
+++ client/src/Components/Header/Header.styles.js
@@ -36,8 +36,11 @@ export const StyledHeader = styled.header`
   z-index: 1;
 `;
 
-/*export const MenuForPhones = styled.label`
+// MOBILE
+
+export const MenuForPhones = styled.label`
   background-color: var(--color-dark-translucent);
+  padding: 2%;
   position: fixed;
   cursor: pointer;
   transition: left .23s;
@@ -45,4 +48,40 @@ export const StyledHeader = styled.header`
   -webkit-backface-visibility: hidden;
   z-index: 3;
   width: 7%;
-`;*/
+`;
+
+export const MenuInsides = styled.ul`
+  display: block;
+  position: fixed;
+  list-style:none;
+  padding: 3%;
+  margin: 0;
+  box-sizing: border-box;
+  width: 7%;
+  background-color: #eee;
+  height: 100%;
+  top: 0;
+  left: -7%;
+  transition: left .2s;
+  z-index: 2;
+  -webkit-transform: translateZ(0);
+  -webkit-backface-visibility: hidden;
+`;
+
+export const SpanStyle = styled.span`
+  display: block;
+  height: 3px;
+  background-color: #fff;
+  margin: 5px 0 0;
+  transition: all .1s linear .23s;
+  position: relative;
+`;
+
+export const In = styled.input`
+  &:checked {
+    left: 1.9%
+  }
+  display: none;
+`;
+
+
