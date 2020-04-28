eaf90956aea2c9bdcccf3541268b8459ccdc0f56
M1M0G <mixail233442@gmail.com>
2020-04-28 18:54:34 +0500

    Update SendMailerRoute.js

--- server/routes/SendMailerRoute.js
+++ server/routes/SendMailerRoute.js
@@ -1,19 +1,19 @@
-var express = require('express');
-const {SendMail}= require('../workers/SendMailer.js');
-var router = express.Router();
-router.use(express.json());
-router.use(express.urlencoded());
-//Использую предложенный тестировщик почты в данный момент. Если будем менять,
-//нужно почитать информацию здесь: https://nodemailer.com/usage/using-gmail/
-//и здесь: https://nodemailer.com/smtp/
-//let testAccount = await nodemailer.createTestAccount();
+// var express = require('express');
+// const {SendMail}= require('../workers/SendMailer.js');
+// var router = express.Router();
+// router.use(express.json());
+// router.use(express.urlencoded());
+// //Использую предложенный тестировщик почты в данный момент. Если будем менять,
+// //нужно почитать информацию здесь: https://nodemailer.com/usage/using-gmail/
+// //и здесь: https://nodemailer.com/smtp/
+// //let testAccount = await nodemailer.createTestAccount();
 
-router.post('/', SendMail);
+// router.post('/', SendMail);
 
-//module.exports =  async function send(mail){
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+// //module.exports =  async function send(mail){
+//       // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     
-    //main().catch(console.error);
-//send();
+//     //main().catch(console.error);
+// //send();
 
-module.exports = router;
+// module.exports = router;
