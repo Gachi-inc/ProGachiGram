135a9b5ebf4975597504497324ac7e971cfaa149
M1M0G <mixail233442@gmail.com>
2020-04-28 18:58:08 +0500

    Try 3

--- server/workers/SendMailer.js
+++ server/workers/SendMailer.js
@@ -1,39 +1,39 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-async function SendMail(req, res, next){
+// async function SendMail(req, res, next){
 
 
-    const {name, email, password, password2 } = req.body;
-    let testEmailAccount = await nodemailer.createTestAccount();
-    let transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
-      secure: false,
-      auth: {
-        user: testEmailAccount.user,
-        pass: testEmailAccount.pass
-      }
-    });
+//     const {name, email, password, password2 } = req.body;
+//     let testEmailAccount = await nodemailer.createTestAccount();
+//     let transporter = nodemailer.createTransport({
+//       host: 'smtp.ethereal.email',
+//       port: 587,
+//       secure: false,
+//       auth: {
+//         user: testEmailAccount.user,
+//         pass: testEmailAccount.pass
+//       }
+//     });
     
-    let result = await transporter.sendMail({
-      from: '"Node js" <nodejs@example.com>',
-      to: email,
-      subject: "I hope this is work!",
-      text: "This message was sent from Node js server.",
-      html: "This <i>message</i> was sent from <strong>Node js</strong> server."
-    });
+//     let result = await transporter.sendMail({
+//       from: '"Node js" <nodejs@example.com>',
+//       to: email,
+//       subject: "I hope this is work!",
+//       text: "This message was sent from Node js server.",
+//       html: "This <i>message</i> was sent from <strong>Node js</strong> server."
+//     });
     
-    console.log("Message sent: %s", result.messageId);
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(result));
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+//     console.log("Message sent: %s", result.messageId);
+//       // Preview only available when sending through an Ethereal account
+//       console.log("Preview URL: %s", nodemailer.getTestMessageUrl(result));
+//       // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     
 
 
 
-}
+// }
 
 
-module.exports =  {
-    SendMail
-}
+// module.exports =  {
+//     SendMail
+// }
