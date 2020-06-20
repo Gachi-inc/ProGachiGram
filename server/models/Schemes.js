2bb9caba634b19d690b927230198f70a801529ef
Komodood <36847935+Komodood@users.noreply.github.com>
2020-06-20 14:35:36 +0500

    isOnline сделали

--- server/models/Schemes.js
+++ server/models/Schemes.js
@@ -42,12 +42,12 @@ const userModelScheme = new Schema({
 }
 );
 
-userModelScheme.virtual('isOnline').get(function(){
-  return differenceInMinutes(parseISO(new Date()), this.last_seen) < 5;
-})
-
 userModelScheme.set('toJSON', {
   virtuals: true,
+})
+
+userModelScheme.virtual('isOnline').get(function(){
+  return differenceInMinutes(this.last_seen,(new Date())) < 5;
 })
 
 userModelScheme.pre('save', async function (next) {
